14d2198ca010c7fe2d2ebb0fb2f389187a5a9d37
muteeb <muteebahmedkamboh44@gmail.com>
2023-06-09 16:52:23 +0500

    Frequency Updated

--- routes/CronJobRoutes.js
+++ routes/CronJobRoutes.js
@@ -4,25 +4,25 @@ var FN = require('../CronFunctions/functions');
 var {CONFIG} = require('../config');
 
 if (CONFIG.CRON === "on") {
-    var PullXMLToMongoObject = new CronJob('01 */12 * * *', function () {
+    var PullXMLToMongoObject = new CronJob('01 */3 * * *', function () {
         console.log("Getting XML TO Mongo Cron Running Start");
         FN.XMLToMongo();
     }, null, true, TIMEZONE);
     PullXMLToMongoObject.start();
 
-    var ScrapBarbie = new CronJob('20 */12 * * *', function () {
+    var ScrapBarbie = new CronJob('20 */3 * * *', function () {
         console.log("Scrap Barbie Cron Running Start");
         FN.ScrapBarbie();
     }, null, true, TIMEZONE);
     ScrapBarbie.start();
 
-    var ScrapComy = new CronJob('35 */12 * * *', function () {
+    var ScrapComy = new CronJob('35 */3 * * *', function () {
         console.log("Scrap Comy Cron Running Start");
         FN.ScrapComy();
     }, null, true, TIMEZONE);
     ScrapComy.start();
 
-    var ScrapEvenTim = new CronJob('50 */12 * * *', function () {
+    var ScrapEvenTim = new CronJob('50 */3 * * *', function () {
         console.log("Scrap EvenTim Cron Running Start");
         FN.ScrapEvenTim();
     }, null, true, TIMEZONE);
