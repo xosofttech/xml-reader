3a5ef66f6a05a0d40a807e000d9ead2f4d5ab87a
muteeb <muteebahmedkamboh44@gmail.com>
2023-07-17 18:34:24 +0500

    Cron Push

--- routes/LocalCronJobRoutes.js
+++ routes/LocalCronJobRoutes.js
@@ -3,7 +3,7 @@ var CronJob = require('cron').CronJob;
 var FN = require('../CronFunctions/local-functions');
 var {CONFIG} = require('../config');
 
-if (CONFIG.CRON === "offf") {
+if (CONFIG.CRON === "on") {
     var PullFetchCities = new CronJob('01 */3 * * *', function () {
         console.log("Getting City Running Start");
         FN.FetchDetails();
@@ -12,4 +12,4 @@ if (CONFIG.CRON === "offf") {
 }
 
 /*Test Funtions to check Crons*/
-//FN.FetchDetails();
+FN.FetchDetails();
