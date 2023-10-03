244087e0e4573fa8461b50430b6e396fefabeea9
muteeb <muteebahmedkamboh44@gmail.com>
2023-10-03 18:53:40 +0500

    Sports Data added

--- routes/LocalCronJobRoutes.js
+++ routes/LocalCronJobRoutes.js
@@ -3,7 +3,7 @@ var CronJob = require('cron').CronJob;
 var FN = require('../CronFunctions/local-functions');
 var {CONFIG} = require('../config');
 
-if (CONFIG.CRON === "offff") {
+if (CONFIG.CRON === "on") {
     var PullFetchCities = new CronJob('01 */1 * * *', function () {
         console.log("Getting City Running Start");
         FN.FetchDetails();
